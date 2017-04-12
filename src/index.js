33005208757bb24c1246624272480538efda290e
cool-Blue <cool.blue@y7mail.com>
2017-04-13 03:31:59 +1000

    Feature  - add import to worker module
     NOT WORKING YET
     todo
      - wrap the define expression in function inject{ }
      - include the startup sequencing in the workerhelper

--- src/index.js
+++ src/index.js
@@ -1,19 +1,49 @@
 var fs = require('fs'),
     path = require('path'),
+    UglifyJS = require('uglify-js'),
     paths = new Map();
 
-module.exports = function () {
+module.exports = function (options) {
+    options = options || {};
+    include = options.include || [];
+    var name = 'bundle-worker';
+    var plugins;
+    function resolver(importee, importer) {
+        var resolved;
+        if (Array.isArray(plugins) && plugins.length > 0) {
+            resolved = plugins.filter(function(p) {
+                return p.name !== name && !!p.resolveId;
+            })
+                .reduce(function(acc, p) {
+                    return acc || p.resolveId(importee, importer);
+                }, false);
+        }
+
+        return resolved || path.resolve(path.dirname(importer), importee);
+    }
     return {
+        name: name,
+        options: function (opts) {
+            plugins = opts.plugins;
+        },
         resolveId: function (importee, importer) {
             if (importee === 'rollup-plugin-bundle-worker') {
                 return path.resolve(__dirname, 'workerhelper.js');
             }
             else if (importee.indexOf('worker!') === 0) {
                 var name = importee.split('!')[1],
-                    target = path.resolve(path.dirname(importer), name);
+                    target = resolver(name, importer);
 
                 paths.set(target, name);
                 return target;
+            }
+            else if (include.indexOf(importee) >= 0) {
+                var target = resolver(importee, importer);
+                paths.set(target, importee);
+                return target;
+            }
+            else {
+                return null;
             }
         },
 
@@ -26,13 +56,32 @@ module.exports = function () {
                 return;
             }
 
+            var workerCode = fs.readFileSync(id, 'utf-8');
+            var ast = UglifyJS.parse(workerCode);
+
+            workerCode = workerCode.replace(/^import inject from 'inject'/,
+                `function inject(req, imports, module) {
+                    return function inject(baseURL) {
+                        let reqUrl = baseURL + req;
+                        let importsURL = imports.map(function (u) {
+                            return baseURL + u;
+                        });
+                        importScripts(reqUrl);
+                        require(['require'].concat(importsURL),
+                            function () {
+                                module.apply(this, Array.from(arguments).slice(1))
+                            });
+                    }
+                }`
+            );
+
             var code = [
-                    `import shimWorker from 'rollup-plugin-bundle-worker';`,
-                    `export default new shimWorker(${JSON.stringify(paths.get(id))}, function (window, document) {`,
-                    `var self = this;`,
-                    fs.readFileSync(id, 'utf-8'),
-                    `\n});`
-                ].join('\n');
+                `import shimWorker from 'rollup-plugin-bundle-worker';`,
+                `export default new shimWorker(${JSON.stringify(paths.get(id))}, function (window, document) {`,
+                `var self = this;`,
+                workerCode,
+                `\n});`
+            ].join('\n');
 
             return code;
         }
